5dceb6da667c4899c00ca4b354f0f0e110cfa026
Shayron <shayron.aguiar@gmail.com>
2024-03-18 19:04:18 -0300

    chore: fix indentation

--- apps/client/app/nova-rota/page.tsx
+++ apps/client/app/nova-rota/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
-	return (
-		<div className="h-full flex">
-			<h1>Hello, Home page!</h1>
-		</div>
-	);
+  return (
+    <div className="h-full flex">
+      <h1>Hello, Home page!</h1>
+    </div>
+  );
 }
